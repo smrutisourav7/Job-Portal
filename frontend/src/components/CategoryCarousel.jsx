1a5267b1f28d3e302c0edcabb6416d91319b3c31
smruti sourav panda <smrutisouravpanda@gmail.com>
2024-12-04 02:05:46 +0530

    Initial commit

---
+++ frontend/src/components/CategoryCarousel.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel.jsx";
+import { Button } from "./ui/button";
+import { setSearchedQuery } from "@/redux/jobSlice.js";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const category = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Data Science",
+  "Graphic Designer",
+  "Full Stack Developer",
+];
+const CategoryCarousel = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const searchJobHandler = (query) => {
+    dispatch(setSearchedQuery(query));
+    navigate("/browse");
+  };
+  return (
+    <div>
+      <Carousel className="w-full max-w-xl mx-auto my-20">
+        <CarouselContent>
+          {category.map((cat, index) => (
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+              <Button
+                onClick={() => searchJobHandler(cat)}
+                className="rounded-full"
+                variant="outline"
+              >
+                {cat}
+              </Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
+
+export default CategoryCarousel;
