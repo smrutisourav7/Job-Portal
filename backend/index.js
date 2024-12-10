c760b553143a6fd9f2929d4975fc7d8fb5236bfa
smruti sourav panda <smrutisouravpanda@gmail.com>
2024-12-10 13:19:50 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5174/", credentials: true }));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
