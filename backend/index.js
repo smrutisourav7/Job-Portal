0e95f9e33e99e1989e5afe990a8ee0af6ceff19a
smruti sourav panda <smrutisouravpanda@gmail.com>
2024-12-11 23:59:44 +0530

    vercel.json added

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,31 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(
+//   cors({
+//     origin: "https://job-portal-gupa.vercel.app",
+//     credentials: true,
+//     optionsSuccessStatus: 200,
+//   })
+// );
+app.use(
+  cors({
+    origin: "https://job-portal-web-dvf6.onrender.com",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://job-portal-gupa.vercel.app",
+//       "https://job-portal-q6dj.onrender.com",
+//     ], // Allow multiple origins
+//     credentials: true, // Allow credentials (cookies)
+//   })
+// );
+
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
